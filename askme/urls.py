8da0b4a7a34d10a4441aa877a8c02e1eca145fe2
Artem Tikhonenko <tinartem@gmail.com>
2022-11-13 20:46:04 +0300

    WIP

--- askme/urls.py
+++ askme/urls.py
@@ -4,13 +4,14 @@ from . import views
 
 urlpatterns = [
     path("", views.main_view, name="main_view"),
-    path("ask/", views.ask_view, name="ask_view"),
     path("hot/", views.hot_view, name="hot_view"),
+    path("settings/<int:user_id>", views.settings_view, name="settings_view"),
     path("tag/<slug:tag_name>", views.tag_view, name="tag_view"),
-    # path("tag/", views.tag_view, name="tag_view"),
+    path("question/<int:question_id>", views.question_view, name="question_view"),
+    path("ask/", views.ask_view, name="ask_view"),
+
     path("login/", views.login_view, name="login_view"),
     path("signup/", views.signup_view, name="signup_view"),
-    path("question/<int:question_id>", views.question_view, name="question_view"),
-    # path("question/", views.question_view, name="question_view"),
+    path("logout/", views.logout_view, name="logout_view"),
     # re_path(r'^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[\w-]+)/$', views.article_detail),
 ]
--- askme/views.py
+++ askme/views.py
@@ -1,15 +1,33 @@
 from django.http import HttpResponse
 from django.template import loader
 
+mock_users = [
+    {"id": "1", "avatar_url": "https://github.com/mdo.png", "name": "Dr. Pepper"},
+    {"id": "2", "avatar_url": "", "name": "Artem"},
+]
+
+mock_tags = [
+    {"id": 1, "human_name": "Pascal", "internal_name": "pascal"},
+    {"id": 3, "human_name": "C++", "internal_name": "cplusplus"},
+    {"id": 15, "human_name": "Python", "internal_name": "python"},
+]
+
+mock_questions = [
+    {"id": "675", "asker": mock_users[1], "title": "Test Title", "rating": 15, "my_rating": 1, "answers_cnt": 5, "tags": [mock_tags[0], mock_tags[1]], "teaser":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+]
+
+
 
 def main_view(request):
     template = loader.get_template("askme/index.html")
-    context = {"user": {"is_logged": True}}
+    context = {"tab": "new", "user": mock_users[0], "questions": mock_questions}
     return HttpResponse(template.render(context, request))
 
 
 def hot_view(request):
-    return HttpResponse("Hello, world. Hot")
+    template = loader.get_template("askme/index.html")
+    context = {"tab": "hot", "user": mock_users[0], "questions": mock_questions}
+    return HttpResponse(template.render(context, request))
 
 
 def tag_view(request, tag_name):
@@ -30,3 +48,11 @@ def signup_view(request):
 
 def ask_view(request):
     return HttpResponse("Hello, world. Ask")
+
+
+def settings_view(request, user_id):
+    return HttpResponse("Hello, world. Settings {}".format(user_id))
+
+
+def logout_view(request):
+    return HttpResponse("Hello, world. Logout")
